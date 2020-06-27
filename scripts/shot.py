df92e5d35796d4a1a03b5b2486189465b9cbc301
Henrique <henriquedelima92@hotmail.com>
2020-06-27 20:46:29 -0300

    added width and height variables to shot object.

--- scripts/shot.py
+++ scripts/shot.py
@@ -2,11 +2,14 @@ import pygame
 from scripts import constant
 from scripts.utilities import Vector2
 
+WIDTH = 3
+HEIGHT = 10
+
 class Shot(pygame.sprite.Sprite):
 
     def __init__(self, position, direction):
         super(Shot, self).__init__()
-        self.surf = pygame.Surface((3,10))
+        self.surf = pygame.Surface((WIDTH,HEIGHT))
         self.surf.fill((0,255,0))
         self.rect = self.surf.get_rect()
         self.position = Vector2(position.x, position.y)
