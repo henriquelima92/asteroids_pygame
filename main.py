195fe68e78e90a064ddc85c11cc1ce07ca4f4b1c
Henrique <henriquedelima92@hotmail.com>
2020-06-29 20:35:02 -0300

    testing how to implement the ui system.

--- main.py
+++ main.py
@@ -18,6 +18,14 @@ def _draw(screen):
     _asteroid_controller.draw(screen)
     _player.draw(screen)
 
+    font = pygame.font.Font('Atari.ttf', 25) 
+    text = font.render('Points: ', True, white) 
+
+    textRect = text.get_rect()  
+    textRect.center = (constant.SCREEN_WIDTH // 2, constant.SCREEN_HEIGHT // 2) 
+
+    _screen.blit(text, textRect)
+
     pygame.display.update()
 
 def _inputs():
@@ -30,7 +38,7 @@ def _inputs():
 
 def _game_loop():
 
-    while _is_running == True:
+    while _is_running == True: 
         _inputs()
         _update()
         _draw(_screen)
@@ -41,11 +49,17 @@ def _game_loop():
 if __name__ == '__main__':
     global _screen
 
+    black = (0, 0, 0) 
+    blue = (0, 0, 128)
+    white = (255, 255, 255)
+
     pygame.init()
     _screen = pygame.display.set_mode([constant.SCREEN_WIDTH,constant.SCREEN_HEIGHT])
-    _asteroid_controller = Asteroid_Controller()
 
+
+    _asteroid_controller = Asteroid_Controller()
     _player = Player()
+
 
     _game_loop()
     
